155d33f91b29a3d6fe7e5f7d952748da036938df
deeks <you@example.com>
2025-03-04 00:31:09 +0100

    dashboard

---
+++ src/components/PredictionMarketDashboard.tsx
@@ -0,0 +1,8 @@
+export default function PredictionMarketDashboard() {
+    return (
+        <div className="min-h-screen flex flex-col">
+            <div className="flex-grow container mx-auto p-4">
+            </div>
+        </div>
+    );
+}
