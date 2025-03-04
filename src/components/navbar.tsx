caf4d45cdb20ad24d2c505853cf5519aef2143c7
deeks <you@example.com>
2025-03-04 11:13:33 +0100

    navbar claim button

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,33 @@
 "use client";
 
-import { ConnectButton, lightTheme } from "thirdweb/react";
+import { ConnectButton, lightTheme, useActiveAccount } from "thirdweb/react";
 import { client } from "@/app/client";
 import { baseSepolia } from "thirdweb/chains";
 import { inAppWallet } from "thirdweb/wallets";
 import { tokenContractAddress } from "@/constants/contracts";
+import { useState } from "react";
+import { Loader2 } from "lucide-react";
+
 
 export function Navbar() {
+    const account = useActiveAccount();
+    const [isClaiming, setIsClaiming] = useState(false);
+
     return (
         <div className="flex items-center justify-between mb-6">
             <h1 className="text-3xl font-bold">Simple Predicition Market</h1>
             <div className="flex items-center gap-2">
+
+                {account && (
+                    <button>
+                        {isClaiming ? (
+                            <>
+                            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                            Claiming...
+                            </>
+                        ) : ("Claim tokens")}
+                    </button>
+                )}
 
                 <ConnectButton client={client} theme={lightTheme()} chain={baseSepolia} connectButton={{ label: "Sign In" }} wallets={[inAppWallet()]}
                 accountAbstraction={{
