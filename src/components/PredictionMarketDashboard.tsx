2d207d31bb2aec384a1d0e02d7e9fab9123f2710
deeks <you@example.com>
2025-03-04 16:19:33 +0100

    skeleton cards

--- src/components/PredictionMarketDashboard.tsx
+++ src/components/PredictionMarketDashboard.tsx
@@ -1,7 +1,8 @@
 import { useReadContract } from "thirdweb/react";
 import { Navbar } from "./navbar";
 import { predictionMarketContract } from "@/constants/contracts";
-import { Tabs, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
+import { MarketCardSkeleton } from "./skeletonCard";
 
 export default function PredictionMarketDashboard() {
     const { data: marketCount, isLoading: isLoadingMarketCount } = useReadContract({
@@ -9,6 +10,10 @@ export default function PredictionMarketDashboard() {
         method: "function marketcount() view returns (uint256)",
         params: []
     });
+
+    const skeletonCards = Array.from({ length: 6 }, (_, index) => (
+        <MarketCardSkeleton key={index} />
+    ));
 
     return (
         <div className="min-h-screen flex flex-col">
@@ -19,6 +24,44 @@ export default function PredictionMarketDashboard() {
                         <TabsTrigger value="active">Active</TabsTrigger>
                         <TabsTrigger value="pending">Pending Resolution</TabsTrigger>
                         <TabsTrigger value="resolved">Resolved</TabsTrigger>
+                        {
+                            isLoadingMarketCount ? (
+
+                             <TabsContent value="active" className="mt-6">
+                                <div className="grid grid-col-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
+                                    { skeletonCards }
+                                </div>
+                            </TabsContent>
+                            ) : (
+                                <>
+
+                                <TabsContent value="active" className="mt-6">
+                                    <div className="grid grid-col-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
+                                        {Array.from({ length: Number(marketCount) }, (_, index) => (
+                                            <></>
+                                        ))}
+                                    </div>
+                                </TabsContent>  
+
+                                <TabsContent value="pending" className="mt-6">
+                                    <div className="grid grid-col-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
+                                        {Array.from({ length: Number(marketCount) }, (_, index) => (
+                                            <></>
+                                        ))}
+                                    </div>
+                                </TabsContent>  
+
+                                <TabsContent value="resolved" className="mt-6">
+                                    <div className="grid grid-col-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
+                                        {Array.from({ length: Number(marketCount) }, (_, index) => (
+                                            <></>
+                                        ))}
+                                    </div>
+                                </TabsContent>  
+                                
+                                </>
+                            )}
+
                     </TabsList>
                 </Tabs>
             </div>
