d96dfe609a7d109d57e834eefd89848bf7439937
deeks <you@example.com>
2025-03-04 11:00:13 +0100

    wallet integration

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,30 @@
-import { ConnectButton } from "thirdweb/react";
+"use client";
+
+import { ConnectButton, lightTheme } from "thirdweb/react";
 import { client } from "@/app/client";
+import { baseSepolia } from "thirdweb/chains";
+import { inAppWallet } from "thirdweb/wallets";
+import { tokenContractAddress } from "@/constants/contracts";
 
 export function Navbar() {
     return (
         <div className="flex items-center justify-between mb-6">
             <h1 className="text-3xl font-bold">Simple Predicition Market</h1>
             <div className="flex items-center gap-2">
-                <ConnectButton
-                client={client}/>
+
+                <ConnectButton client={client} theme={lightTheme()} chain={baseSepolia} connectButton={{ label: "Sign In" }} wallets={[inAppWallet()]}
+                accountAbstraction={{
+                    chain: baseSepolia,
+                    sponsorGas: true,
+                }}
+                detailsButton={{
+                    displayBalanceToken: {
+                        [baseSepolia.id]: tokenContractAddress
+                    }
+                }}
+                
+                />
             </div>
-        </div>
+        </div> 
     )
 }
